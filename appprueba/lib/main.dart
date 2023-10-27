26817f5335962364878d8dd353defc101d2b8e8d
Benjamin <benja.gonzal.ez460@gmail.com>
2023-10-26 22:43:52 -0300

    letras

--- appprueba/lib/main.dart
+++ appprueba/lib/main.dart
@@ -99,7 +99,7 @@ class _LoginPageState extends State<LoginPage> {
               const Text(
                 'Sabores de Santa Rosa',
                 style: TextStyle(
-                  fontFamily: '',
+                  fontFamily: 'montserrat',
                   fontSize: 40,
                   color: Color.fromARGB(255, 255, 255, 255),
                 ),
